f786477f7e58fcdcd99f2391fbbc8539216d1142
Bassim <bassim.matar@gmail.com>
2015-10-28 21:53:24 +0100

    Update connexion_base.php

--- connexion_base.php
+++ connexion_base.php
@@ -8,17 +8,17 @@ try
 	$dbname = 'chat';
 	$user = 'chat_user';
 	$password = 'dg99qJ3qnWW2';
-
-
+	
+	
 	/* ENCRYPTED PASSWORD (OPTIONAL) 
 	include_once 'other/get_passowrd_decrypt.php';
 	$password = getPassword_decrypt();
 	*/
 
-
+	
 	// CONNEXION
-    global $bdd;
-    $bdd = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8', $user, $password);
+	global $bdd;
+	$bdd = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8', $user, $password);
 
 }
 
@@ -26,8 +26,8 @@ try
 // ERROR
 catch (PDOException $e)
 {
-    print "Erreur !: " . $e->getMessage() . "<br/>";
-    die();
+	print "Erreur !: " . $e->getMessage() . "<br/>";
+	die();
 }
 
 
